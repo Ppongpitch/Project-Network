d188c11960e7299b0a86576630a728f5d5f5196c
Japokkrong <jajaongjan@gmail.com>
2025-11-15 18:42:57 +0700

    change hostname to localhos

--- server.ts
+++ server.ts
@@ -5,7 +5,7 @@ import { Server as SocketIOServer } from 'socket.io'
 import { prisma } from './lib/prisma/client'
 
 const dev = process.env.NODE_ENV !== 'production'
-const hostname = '0.0.0.0'  // Listen on all network interfaces
+const hostname = 'localhost'
 const port = 3000
 
 const app = next({ dev, hostname, port })
